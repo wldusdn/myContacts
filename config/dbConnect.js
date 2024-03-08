20b01e9615045f650cdc80c8ccd212d3c214e4da
wldusdn <wldusdngg@naver.com>
2024-03-08 12:07:44 +0900

    atlas로 수정

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const dbConnect = async()=>{
   try{
-    const connect = await mongoose.connect(process.env.DB_LOCAL_URL);
+    const connect = await mongoose.connect(process.env.DB_ATLAS_URL);
     console.log('DB 연결됨!');
   }catch(err){
     console.log(err);
